984e19ee34412a81086fc72e762c8654f7aef090
TheMisfit68 <janverrept@me.com>
2025-03-29 20:55:35 +0100

    Added Clamping with a return value instead of mutating the type

--- Sources/JVEmbedded/JVSwiftCoreTypes/Clamping.swift
+++ Sources/JVEmbedded/JVSwiftCoreTypes/Clamping.swift
@@ -1,5 +1,9 @@
 extension Comparable {
-    func clamped(to range: ClosedRange<Self>) -> Self {
-        return min(max(self, range.lowerBound), range.upperBound)
-    }
+	mutating func clamp(to range: ClosedRange<Self>) {
+		self = min(max(self, range.lowerBound), range.upperBound)
+	}
+	
+	func clamped(to range: ClosedRange<Self>) -> Self {
+		return min(max(self, range.lowerBound), range.upperBound)
+	}
 }

e157bbe941038afc31197a461de178246206295c
TheMisfit68 <janverrept@me.com>
2025-08-25 14:04:17 +0200

    Added MIN and MAX to comparable

--- Sources/JVEmbedded/SwiftCore/Comparable.swift
+++ Sources/JVEmbedded/SwiftCore/Comparable.swift
@@ -7,3 +7,14 @@ extension Comparable {
 		return min(max(self, range.lowerBound), range.upperBound)
 	}
 }
+
+
+@inlinable
+public func MIN<T: Comparable>(_ x: T, _ y: T) -> T {
+	return y < x ? y : x
+}
+
+@inlinable
+public func MAX<T: Comparable>(_ x: T, _ y: T) -> T {
+	return x < y ? y : x
+}
